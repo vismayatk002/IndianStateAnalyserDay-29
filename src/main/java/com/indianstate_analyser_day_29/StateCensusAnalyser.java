e27debfceaabe83eb5f9346f73726c98de9c25df
vismayatk002 <vismayatk002@gmail.com>
2021-08-28 22:44:08 +0530

    Added welcome message

---
+++ src/main/java/com/indianstate_analyser_day_29/StateCensusAnalyser.java
@@ -0,0 +1,9 @@
+package com.indianstate_analyser_day_29;
+
+public class StateCensusAnalyser {
+	
+    public static void main( String[] args ) {
+    	
+        System.out.println( "Welcome to Indian State Census Analyser" );
+    }
+}
---
+++ src/test/java/com/indianstate_analyser_day_29/AppTest.java
@@ -0,0 +1,20 @@
+package com.indianstate_analyser_day_29;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
